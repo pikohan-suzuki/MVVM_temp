579918d465758669fcaed28f9b0e773498e6e8ea
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-26 12:59:47 +0900

    add navigation

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/mvvm_temp/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/mvvm_temp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.amebaownd.pikohan_nwiatori.mvvm_temp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.text.observe(this, Observer {
+            Log.d("aaaa",it)
+        })
+
     }
 }
