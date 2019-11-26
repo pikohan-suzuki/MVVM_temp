df64ef6cc99b81d0cd6a51447bba91c423496d80
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-26 15:36:45 +0900

    remove_answer

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/mvvm_temp/MainFragment.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/mvvm_temp/MainFragment.kt
@@ -5,19 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import com.amebaownd.pikohan_nwiatori.mvvm_temp.databinding.FragmentMainBinding
 
 class MainFragment :Fragment(){
 
-    private lateinit var binding :FragmentMainBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        binding = FragmentMainBinding.inflate(inflater,container,false).apply {
-            lifecycleOwner = viewLifecycleOwner
-            this.viewModel = viewModel
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_main,container,false)
     }
 }
--- app/src/main/java/com/amebaownd/pikohan_nwiatori/mvvm_temp/MainViewModel.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/mvvm_temp/MainViewModel.kt
@@ -4,13 +4,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel :ViewModel(){
-    var text = MutableLiveData<String>()
 
-    fun onAaaaButtonClicked(){
-        text.value = "aaaa"
-    }
 
-    fun onClearButtonClicked(){
-        text.value = ""
-    }
 }
